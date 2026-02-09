eeb8672f45984929f91af188f3fb93af0d0bd7a4
Pranjal sharma <sharmaa29234@gmail.com>
2026-02-09 09:58:40 +0530

    Fix auth.py to use API_URL env var

--- frontend/auth.py
+++ frontend/auth.py
@@ -1,7 +1,8 @@
 import requests
 import streamlit as st
 
-API_URL = "http://localhost:8000"
+import os
+API_URL = os.getenv("API_URL", "http://localhost:8000")
 
 def login_user(username, password):
     try:
